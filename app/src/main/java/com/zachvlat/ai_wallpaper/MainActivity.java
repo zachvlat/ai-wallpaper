e3a9dcde6fa428c15ba20f44d88e823c23d32bdc
Zacharias Vlatakis <zachariasvlatakis@gmail.com>
2024-10-08 16:34:44 +0300

    Update MainActivity.java

--- app/src/main/java/com/zachvlat/ai_wallpaper/MainActivity.java
+++ app/src/main/java/com/zachvlat/ai_wallpaper/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
     private void fetchWallpapers() {
         new Thread(() -> {
             try {
-                URL url = new URL("https://gist.githubusercontent.com/zachvlat/4009ec76cbaa9c5134b3831dc91a4414/raw/9f66011f81ad41964b38b23d95a9ac0bf9b4ff31/fullImageUrls.json");
+                URL url = new URL("https://zachinvalidation.netlify.app/fullImageUrls.json");
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 StringBuilder stringBuilder = new StringBuilder();
